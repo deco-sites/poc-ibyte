13a87d79be3165455f0a16788f645bf15dba9f56
gustavo-vasconcellos <gustavo.vasconcellos@hp.com>
2023-03-18 12:47:57 -0300

    feat(Alert): export icons

--- sections/Alert.tsx
+++ sections/Alert.tsx
@@ -1,4 +1,3 @@
 export { default } from "$store/components/ui/Alert.tsx";
-export type { AvailableIcons } from '$store/components/ui/Icon.tsx';
+export type { AvailableIcons } from "$store/components/ui/Icon.tsx";
 export type { Alert, Props } from "../components/ui/Alert.tsx";
-
