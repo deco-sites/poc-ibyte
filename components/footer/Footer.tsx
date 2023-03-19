fc30d1da95987b3004fce546c2543837dabb62f1
gustavo-vasconcellos <gustavo.vasconcellos@hp.com>
2023-03-19 16:06:32 -0300

    feat: add footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -59,142 +59,145 @@ function Footer(
 ) {
   return (
     <footer class="w-full bg-white flex flex-col">
-      <Container class="max-w-[1280px] bg-header mb-7 grid grid-cols-[48px_1fr_48px]">
-        <Slider
-          class="gap-6 col-span-full row-start-2 row-end-5 text-white"
-          snap="snap-center sm:snap-start block first:ml-6 sm:first:ml-0 last:mr-6 sm:last:mr-0"
-        >
-          <a
-            href="/"
-            aria-label="Consulte Frete Grátis"
-            class="flex items-center gap-3 py-7"
+      <div class="bg-header w-full mb-7">
+        <Container>
+          <Slider
+            class="gap-6 col-span-full row-start-2 row-end-5 text-white md:justify-around"
+            snap="snap-center sm:snap-start block first:ml-6 sm:first:ml-0 last:mr-6 sm:last:mr-0"
           >
-            <Icon id="Truck" strokeWidth={1} width={30} height={30} />
-            <div>
-              <p class="uppercase whitespace-nowrap font-semibold text-base">
-                Frete Grátis
-              </p>
-              <p class="text-sm whitespace-nowrap font-extralight">Consulte*</p>
-            </div>
-          </a>
-          <a
-            href="/"
-            aria-label="Compre no site e retire na loja"
-            class="flex items-center gap-3 py-7"
-          >
-            <Icon id="MapPin" strokeWidth={1} width={30} height={30} />
-            <div>
-              <p class="uppercase whitespace-nowrap font-semibold text-base">
-                Compra no site
-              </p>
-              <p class="text-sm whitespace-nowrap font-extralight">
-                E retire na loja*
-              </p>
-            </div>
-          </a>
-          <div
-            aria-label="Em até 12 vezes no cartão de crédito*"
-            class="flex items-center gap-3 py-7"
-          >
-            <Icon id="CreditCard" strokeWidth={1} width={30} height={30} />
-            <div>
-              <p class="uppercase whitespace-nowrap font-semibold text-base">
-                Em até 12X
-              </p>
-              <p class="text-sm whitespace-nowrap font-extralight">
-                No cartão de crédito*
-              </p>
-            </div>
-          </div>
-          <a
-            href="/"
-            aria-label="Compre no site e retire na loja"
-            class="flex items-center gap-3 py-7"
-          >
-            <Icon id="ShieldCheck" strokeWidth={1} width={30} height={30} />
-            <div>
-              <p class="uppercase whitespace-nowrap font-semibold text-base">
-                Garantia
-              </p>
-              <p class="text-sm whitespace-nowrap font-extralight">
-                Veja as condições
-              </p>
-            </div>
-          </a>
-        </Slider>
-      </Container>
-      {specialistPhone
-        ? (
-          <Container class="mb-2">
-            <p class="text-sm font-medium text-center mb-2">
-              Fale com o Consultor Especializado
-            </p>
             <a
-              href="https://bit.ly/faleconsultor-ibyte"
-              class="font-heading-2 text-heading-2 gap-2 flex justify-center items-center"
+              href="/"
+              aria-label="Consulte Frete Grátis"
+              class="flex items-center gap-3 py-7 md:justify-center"
             >
-              <Icon
-                id="WhatsApp"
-                width={26}
-                height={26}
-                class="text-[#56b231]"
-              />
-              <span>{specialistPhone}</span>
+              <Icon id="Truck" strokeWidth={1} width={30} height={30} />
+              <div>
+                <p class="uppercase whitespace-nowrap font-semibold text-base">
+                  Frete Grátis
+                </p>
+                <p class="text-sm whitespace-nowrap font-extralight">
+                  Consulte*
+                </p>
+              </div>
             </a>
-          </Container>
-        )
-        : null}
-      <Container class="w-full flex flex-col ">
-        <FooterContainer>
-          {/* Desktop view */}
-          <ul class="hidden sm:flex flex-row gap-20">
-            {primarySections.map((section) => (
-              <li>
-                <div>
-                  <Text variant="heading-3" tone="default">
-                    {section.label}
-                  </Text>
+            <a
+              href="/"
+              aria-label="Compre no site e retire na loja"
+              class="flex items-center gap-3 py-7 md:justify-center"
+            >
+              <Icon id="MapPin" strokeWidth={1} width={30} height={30} />
+              <div>
+                <p class="uppercase whitespace-nowrap font-semibold text-base">
+                  Compra no site
+                </p>
+                <p class="text-sm whitespace-nowrap font-extralight">
+                  E retire na loja*
+                </p>
+              </div>
+            </a>
+            <div
+              aria-label="Em até 12 vezes no cartão de crédito*"
+              class="flex items-center gap-3 py-7 md:justify-center"
+            >
+              <Icon id="CreditCard" strokeWidth={1} width={30} height={30} />
+              <div>
+                <p class="uppercase whitespace-nowrap font-semibold text-base">
+                  Em até 12X
+                </p>
+                <p class="text-sm whitespace-nowrap font-extralight">
+                  No cartão de crédito*
+                </p>
+              </div>
+            </div>
+            <a
+              href="/"
+              aria-label="Compre no site e retire na loja"
+              class="flex items-center gap-3 py-7 md:justify-center"
+            >
+              <Icon id="ShieldCheck" strokeWidth={1} width={30} height={30} />
+              <div>
+                <p class="uppercase whitespace-nowrap font-semibold text-base">
+                  Garantia
+                </p>
+                <p class="text-sm whitespace-nowrap font-extralight">
+                  Veja as condições
+                </p>
+              </div>
+            </a>
+          </Slider>
+        </Container>
+      </div>
 
-                  <ul
-                    class={`flex ${
-                      isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                    } gap-2 pt-2 items-center`}
-                  >
-                    {section.children.map((item) => (
-                      <li>
-                        <SectionItem item={item} />
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </li>
-            ))}
-          </ul>
-
-          {/* Mobile view */}
-          <ul class="flex flex-col sm:hidden sm:flex-row gap-4 pt-2">
-            {primarySections.map((section) => (
-              <li>
-                <Text variant="body" tone="default">
-                  <Text variant="heading-3" class="text-default">
-                    {section.label}
-                  </Text>
-                  <ul
-                    class={`flex ${
-                      isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                    } gap-2 pt-3`}
-                  >
-                    {section.children.map((item) => (
-                      <li>
-                        <SectionItem item={item} />
-                      </li>
-                    ))}
-                  </ul>
+      <Container class="md:(w-full flex justify-start gap-[10rem] mb-8)">
+        {specialistPhone
+          ? (
+            <div class="mb-2 md:self-center whitespace-nowrap">
+              <p class="text-sm font-medium text-center mb-2">
+                Fale com o Consultor Especializado
+              </p>
+              <a
+                href="https://bit.ly/faleconsultor-ibyte"
+                class="font-heading-2 text-heading-2 gap-2 flex justify-center items-center"
+              >
+                <Icon
+                  id="WhatsApp"
+                  width={26}
+                  height={26}
+                  class="text-[#56b231]"
+                />
+                <span>{specialistPhone}</span>
+              </a>
+            </div>
+          )
+          : null}
+        {/* Desktop view */}
+        <ul class="hidden sm:flex flex-row md:self-start gap-[10rem]">
+          {primarySections.map((section) => (
+            <li>
+              <div>
+                <Text variant="heading-3" tone="default">
+                  {section.label}
                 </Text>
-              </li>
-            ))}
-          </ul>
-        </FooterContainer>
+
+                <ul
+                  class={`flex ${
+                    isIcon(section.children[0]) ? "flex-row" : "flex-col"
+                  } gap-2 pt-3`}
+                >
+                  {section.children.map((item) => (
+                    <li>
+                      <SectionItem item={item} />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        {/* Mobile view */}
+        <ul class="flex flex-col sm:hidden sm:flex-row gap-4 pt-2">
+          {primarySections.map((section) => (
+            <li>
+              <Text variant="body" tone="default">
+                <Text variant="heading-3" class="text-default">
+                  {section.label}
+                </Text>
+                <ul
+                  class={`flex ${
+                    isIcon(section.children[0]) ? "flex-row" : "flex-col"
+                  } gap-2 pt-3`}
+                >
+                  {section.children.map((item) => (
+                    <li>
+                      <SectionItem item={item} />
+                    </li>
+                  ))}
+                </ul>
+              </Text>
+            </li>
+          ))}
+        </ul>
       </Container>
       <div class="bg-gray-100">
         <Container class="w-full flex flex-col">
@@ -204,14 +207,14 @@ function Footer(
               {secondarySections.map((section) => (
                 <li>
                   <div>
-                    <p class="text-sm">
+                    <p class="text-sm font-semibold">
                       {section.label}
                     </p>
 
                     <ul
                       class={`flex ${
                         isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                      } gap-2 pt-2`}
+                      } gap-2 pt-3`}
                     >
                       {section.children.map((item) => (
                         <li>
