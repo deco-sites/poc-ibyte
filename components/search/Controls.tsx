0f363f6b9aaabd1bf2db59edf65e3dd23545a85f
gustavo-vasconcellos <gustavo.vasconcellos@hp.com>
2023-03-19 05:20:56 -0300

    style(Controls): organize imports

--- components/search/Controls.tsx
+++ components/search/Controls.tsx
@@ -1,13 +1,12 @@
-import Container from "$store/components/ui/Container.tsx";
-import Button from "$store/components/ui/Button.tsx";
-import Icon from "$store/components/ui/Icon.tsx";
+import type { LoaderReturnType } from "$live/types.ts";
 import Filters from "$store/components/search/Filters.tsx";
 import Sort from "$store/components/search/Sort.tsx";
+import Button from "$store/components/ui/Button.tsx";
+import Container from "$store/components/ui/Container.tsx";
+import Icon from "$store/components/ui/Icon.tsx";
 import Modal from "$store/components/ui/Modal.tsx";
-import Breadcrumb from "$store/components/ui/Breadcrumb.tsx";
 import { useSignal } from "@preact/signals";
 import type { ProductListingPage } from "deco-sites/std/commerce/types.ts";
-import type { LoaderReturnType } from "$live/types.ts";
 
 export interface Props {
   page: LoaderReturnType<ProductListingPage | null>;
